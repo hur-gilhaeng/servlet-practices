7c119576664ae9bd416e94d162b03ca54193309c
R35 <R35@DESKTOP-J04NN5U>
2020-02-18 14:05:41 +0900

    ip수정

--- emaillist01/src/main/java/com/douzone/email/dao/EmaillistDao.java
+++ emaillist01/src/main/java/com/douzone/email/dao/EmaillistDao.java
@@ -109,6 +109,7 @@ public class EmaillistDao {
 			Class.forName("org.mariadb.jdbc.Driver");
 
 			String url = "jdbc:mysql://127.0.0.1:3307/webdb";
+			//String url = "jdbc:mysql://192.168.1.97:3307/webdb";
 			 
 			return DriverManager.getConnection(url, "webdb", "webdb");
 
